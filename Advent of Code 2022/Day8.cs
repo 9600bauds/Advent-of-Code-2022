3cde7860db513e4b2af57b127a62198e8f20004a
Blas Aladio <blasaladio3@gmail.com>
2022-12-07 15:44:20 -0300

    Progress on Day 7 (really just the regex)

--- Advent of Code 2022/Day8.cs
+++ Advent of Code 2022/Day8.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Advent_of_Code_2022
 {
-    internal class Program
+    internal class Day8
     {
-        static void Main(string[] args)
-        {
-            Day7.Run();
-        }
     }
 }
