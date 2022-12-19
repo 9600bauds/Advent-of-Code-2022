61cd79db1256857bf68a099458440c891e7f2c1b
Blas Aladio <blasaladio3@gmail.com>
2022-12-19 01:44:41 -0300

    Solved with pen and paper, pending code solution

---
+++ Advent of Code 2022/libs/GridRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2022.libs
+{
+    public static class GridRenderer
+    {
+        public static void Render(int posx, int posy, char[,] grid)
+        {
+            int oldy = Console.CursorTop;
+            int oldX = Console.CursorLeft;
+            int height = grid.GetLength(1);
+            int width = grid.GetLength(0);
+            for (int y = height - 1; y >= 0; y--)
+            {
+                string temp = "";
+                Console.SetCursorPosition(posx, posy + height - y);
+                for (int x = 0; x <= width - 1; x++)
+                {
+                    temp += grid[x, y];
+                }
+                temp += "\n";
+                Console.Write(temp);
+            }
+            Console.SetCursorPosition(oldX, oldy);
+        }
+    }
+}
